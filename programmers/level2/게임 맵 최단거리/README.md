1693e9071c31475ad2c92a64e43cbc602ac93169
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-10 16:05:50 +0900

    wip: 프로그래머스 lv2 게임맵최단거리 (BFS/DFS) 풀이 중

---
+++ programmers/level2/게임 맵 최단거리/README.md
@@ -0,0 +1,6 @@
+## 게임 맵 최단거리 lv2
+
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/1844 <br />
+풀이 시간: 30분
+
+**어려웠던 점** 
---
+++ programmers/level2/게임 맵 최단거리/solution.py
@@ -0,0 +1,11 @@
+def solution(maps):
+    
+    # maps = n x m 행렬
+    
+    current = maps[0][0]
+    target = maps[len(maps)][len(maps[0])]
+    
+    
+            
+    answer = 0
+    return answer
