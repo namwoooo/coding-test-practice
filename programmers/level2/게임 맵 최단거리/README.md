3499aa14fde5d2976578d427d22e0d0ad17c78ca
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-11 18:50:24 +0900

    sovle: 프로그래머스 lv2 게임맵최단거리 (BFS/DFS)

--- programmers/level2/게임 맵 최단거리/README.md
+++ programmers/level2/게임 맵 최단거리/README.md
@@ -1,6 +1,9 @@
 ## 게임 맵 최단거리 lv2
 
 링크: https://school.programmers.co.kr/learn/courses/30/lessons/1844 <br />
-풀이 시간: 30분
+풀이 시간: 30분 / 41분
 
 **어려웠던 점** 
+- 최단 거리를 구할 때는 BFS를 떠올려야 한다
+- queue를 어떻게 구성할지 -> (x, y, dist)
+- 상하좌우를 탐색하는데 각각의 조건을 다르게 할 것 (상, 좌는 각 row, col이 0 이상이도록 / 하, 우는 각 row, col이 n, m 미만이도록 설정)
--- programmers/level2/게임 맵 최단거리/solution.py
+++ programmers/level2/게임 맵 최단거리/solution.py
@@ -1,11 +1,30 @@
+from collections import deque 
+
 def solution(maps):
+    # answer: 최단 거리 
+    n = len(maps)
+    m = len(maps[0])
     
-    # maps = n x m 행렬
+    visited = [[0] * m for _ in range(n)]  # 방문 기록  n x m
+    q = deque([(0, 0, 1)])  # 초기화(시작점)
     
-    current = maps[0][0]
-    target = maps[len(maps)][len(maps[0])]
-    
-    
+    while q:
+        current = q.popleft()  # 현재 위치
+        x, y, dist = current[0], current[1], current[2]
+        
+        if y - 1 >= 0 and maps[x][y - 1] != 0 and visited[x][y-1] == 0:
+            visited[x][y - 1] = 1
+            q.append((x, y - 1, dist + 1))
+        if y + 1 < m and maps[x][y + 1] != 0 and visited[x][y + 1] == 0:
+            visited[x][y + 1] = 1
+            q.append((x, y + 1, dist + 1))
+        if x - 1 >= 0 and maps[x - 1][y] != 0 and visited[x - 1][y] == 0:
+            visited[x - 1][y] = 1
+            q.append((x - 1, y, dist + 1))
+        if x + 1 < n and maps[x + 1][y] != 0 and visited[x + 1][y] == 0:
+            visited[x + 1][y] = 1
+            q.append((x + 1, y, dist + 1))
             
-    answer = 0
-    return answer
+        if x == n - 1 and y == m - 1:
+            return dist
+    return -1   # queue가 비었는데 목표 지점에 도달하지 못했을 경우
